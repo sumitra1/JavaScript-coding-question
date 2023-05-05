b1ee1f49a0978bcba3e1e30300a7da16087755a3
sumitra1 <sumitrakumarishaw1@gmail.com>
2023-05-05 23:27:02 +0530

    flatten Array

--- practice.js
+++ practice.js
@@ -1,10 +1,41 @@
-let student=[
-    {name:"Piyush",rollNumber:31},
-    {name:"jenny",rollNumber:15},
-    {name:"Kaushal",rollNumber:55}
-]
+const input = {
+  A: "12",
 
-const filterArray=student.map((item,index)=>{
-    return item.name.toUpperCase()
-})
-console.log(filterArray)
+  B: 23,
+
+  C: {
+    P: 23,
+
+    O: {
+      L: 56,
+    },
+
+    // Q: [1, 2],
+  },
+};
+
+
+// { A: '12', B: 23, 'C.P': 23, 'C.O.L': 56, 'C.Q.0': 1, 'C.Q.1': 2 }
+
+const flattenObject=(inputObj,prefix)=>{
+    console.log(prefix);
+    let outputObj={};
+    for(const [key,value] of Object.entries(inputObj)){
+        const newKey=prefix? prefix+'.'+key: key;
+        // console.log(key,value);
+       if(typeof value!=="object"){
+        outputObj[newKey]= value;
+       }else{
+        const outputObj2= flattenObject(value,newKey);  
+        outputObj={...outputObj,...outputObj2};
+       }
+    }
+    return outputObj;  
+}
+
+console.log(flattenObject(input));
+
+
+
+
+
